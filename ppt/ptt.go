4481ea9b0c7957a77e5ee3db758b67480d6c8a6f
jneo8 <james851212@gmail.com>
2017-12-21 00:27:47 +0800

    First req success

--- ppt/ptt.go
+++ ppt/ptt.go
@@ -1,10 +1,39 @@
 package ptt
 
 import (
-    // "net/http"
+    "net/http"
+    "fmt"
+    "io/ioutil"
 )
 
-func Doc(url string) int {
-    // resp, err := http.NewRequest("GET", url, nil)
-    return 0
+const (
+    BASE_URL = "https://www.ptt.cc/bbs/"
+)
+
+func GetDoc(url string) ([]byte, error) {
+    // Add cookie
+    req, err := http.NewRequest("GET", url, nil)
+    cookie := http.Cookie {
+        Name: "over18",
+        Value: "1",
+    }
+    req.AddCookie(&cookie)
+
+    // Send req
+    resp, err := http.DefaultClient.Do(req)
+
+    // Read Body
+    body, err := ioutil.ReadAll(resp.Body)
+    resp.Body.Close()
+    return body, err
+}
+
+
+func GetArticles(board string) {
+    idx := "/index.html"
+    url := BASE_URL + board + idx
+    fmt.Println(url)
+    body, err := GetDoc(url)
+    fmt.Printf("%s", body)
+    fmt.Printf("%s", err)
 }
--- sample.go
+++ sample.go
@@ -2,10 +2,8 @@ package main
 
 import (
     "./ppt"
-    "fmt"
 )
 
 func main() {
-    resp := ptt.Doc("123")
-    fmt.Println(resp)
+    ptt.GetArticles("Gossiping")
 }
