7c01389491c860fbd7cb10f55b23e7f5c73d8446
jneo8 <james851212@gmail.com>
2017-12-20 23:44:31 +0800

    try local import

---
+++ ppt/ptt.go
@@ -0,0 +1,10 @@
+package ptt
+
+import (
+    // "net/http"
+)
+
+func Doc(url string) int {
+    // resp, err := http.NewRequest("GET", url, nil)
+    return 0
+}
---
+++ sample.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "./ppt"
+    "fmt"
+)
+
+func main() {
+    resp := ptt.Doc("123")
+    fmt.Println(resp)
+}
