1a02f9c0cc7010580558ca8d804bd6c6eb761daa
jneo8 <james851212@gmail.com>
2018-01-01 19:59:37 +0800

    Use goquery get resp

--- ptt/ptt.go
+++ ptt/ptt.go
@@ -1,80 +1,45 @@
 package ptt
 
 import (
-    "net/http"
     "fmt"
     "log"
-    // "io/ioutil"
-    "golang.org/x/net/html"
-    "reflect"
+    "net/http"
+    "github.com/PuerkitoBio/goquery"
 )
 
 const (
     BASE_URL = "https://www.ptt.cc/bbs/"
 )
 
-func GetResp(url string) (*http.Response) {
+func GetResp(url string) (*goquery.Document) {
     // Add cookie
     req, err := http.NewRequest("GET", url, nil)
     if err != nil {
         log.Fatal(err)
     }
-    // cookie := http.Cookie {
-    //     Name: "over18",
-    //     Value: "1",
-    // }
-    // req.AddCookie(&cookie)
 
-    // Send req
+    cookie := http.Cookie{
+        Name:  "over18",
+        Value: "1",
+    }
+    req.AddCookie(&cookie)
+
     resp, err := http.DefaultClient.Do(req)
-
-    // Read Body
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Println(resp.Body)
-    // bytes, err := ioutil.ReadAll(resp.Body)
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-    // text := string(bytes)
-    // fmt.Printf("%T\n", text)
-    defer resp.Body.Close()
-    fmt.Println(resp.Body)
 
-    z := html.NewTokenizer(resp.Body)
-    tokens_loop:
-        for {
-            tt := z.Next()
-            switch tt {
-                case html.ErrorToken:
-                    fmt.Println("End")
-                    break tokens_loop
-                default:
-                    tn, _ := z.TagName()
-                    t := z.Token()
-                    key, value, more_attr := z.TagAttr()
-                    // TODO
-                    if true {
-                        fmt.Printf("---------\n")
-                        fmt.Printf("tagename: %s data: %s\n", tn, t.Data)
-                        fmt.Printf("key: %s\n", key)
-                        fmt.Printf("value: %s type: %t\n", value, reflect.TypeOf(value))
-                        fmt.Printf("more_attr: %t\n", more_attr)
-                        fmt.Printf("---------\n")
-                    }
+    if resp.StatusCode != http.StatusOK {
+        log.Fatal(resp.StatusCode)
+    }
 
-                    // fmt.Printf("---------\n")
-                    // fmt.Printf("tagename: %s data: %s\n", tn, t.Data)
-                    // fmt.Printf("key: %s\n", key)
-                    // fmt.Printf("value: %s type: %t\n", value, value)
-                    // fmt.Printf("more_attr: %t\n", more_attr)
-                    // fmt.Printf("---------\n")
-            }
-        }
-    return resp
+    doc, err := goquery.NewDocumentFromResponse(resp)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return doc
 }
-
 
 func GetBoardList() {
     url := "https://www.ptt.cc/bbs/hotboards.html"
