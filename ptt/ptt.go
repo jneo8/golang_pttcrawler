8f64d39a2375db35a7be6b4a671b927d6dd3742f
jneo8 <james851212@gmail.com>
2018-01-01 21:46:00 +0800

    Get page1 title and nrec

--- ptt/ptt.go
+++ ptt/ptt.go
@@ -4,12 +4,28 @@ import (
     "fmt"
     "log"
     "net/http"
+    // "math"
+    "strings"
+    "strconv"
     "github.com/PuerkitoBio/goquery"
 )
 
 const (
     BASE_URL = "https://www.ptt.cc/bbs/"
+    HOT_BOARD_URL = "https://www.ptt.cc/bbs/hotboards.html"
 )
+
+type Article struct {
+    ID       string
+    Board    string
+    Title    string
+    Content  string
+    Author   string
+    DateTime string
+    Nrec     int
+    Url      string
+    doc      *goquery.Document
+}
 
 func GetDoc(url string) (*goquery.Document) {
     // Add cookie
@@ -41,23 +57,46 @@ func GetDoc(url string) (*goquery.Document) {
     return doc
 }
 
-func GetHotBoardList() map[string]string {
-    hot_board_list := make(map[string]string)
-    url := "https://www.ptt.cc/bbs/hotboards.html"
-    doc := GetDoc(url)
 
+func GetHotBoardList() map[string]string {
+    doc := GetDoc(HOT_BOARD_URL)
+
+    // Parser
     // Get board name & href
+    hot_board_list := make(map[string]string)
     doc.Find(".board").Each(func(i int, s *goquery.Selection) {
         href, _ := s.Attr("href")
         board_name := s.Find(".board-name").Text()
         hot_board_list[board_name] = href
     })
 
+    // Print
     for name, href := range hot_board_list {
         fmt.Printf("%s: %s\n", name, href)
     }
 
     return hot_board_list
+}
+
+
+func GetArticles(board string) {
+    url := BASE_URL + board + "/index.html"
+    doc := GetDoc(url)
+    // articles := make([]*Article, 0)
+
+    doc.Find(".r-ent").Each(func(i int, s *goquery.Selection) {
+        // article := &Article{Board: board}
+        title := strings.TrimSpace(s.Find(".title").Text())
+        // nrec
+        nrec := s.Find(".nrec")
+        if len(nrec.Nodes) > 0 {
+            nrec_str := nrec.Text()
+            nrec_num, _ := strconv.Atoi(nrec_str)
+
+            fmt.Printf("%d : %s\n", nrec_num, title)
+        }
+
+    })
 
 }
 
