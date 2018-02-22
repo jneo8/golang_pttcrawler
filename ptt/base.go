f3be602743d5da1c131e77c1dad98ff1f7bfef02
jneo8 <james851212@gmail.com>
2018-02-22 14:26:39 +0800

    move base const in base.go

---
+++ ptt/base.go
@@ -0,0 +1,13 @@
+package ptt
+
+const (
+    BASE_URL = "https://www.ptt.cc/bbs/"
+    ARTICLE_BASE_URL = "https://www.ptt.cc"
+    HOT_BOARD_URL = "https://www.ptt.cc/bbs/hotboards.html"
+
+    // default value
+    DEFAULT_AUTHOR_NAME = "DEFAULT_AUTHOR"
+    DEFAULT_TITLE = "DEFAULT_TITLE"
+)
+
+
