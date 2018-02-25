4b39d3f430dccc85e3ea4e92f09d8bb59740f6b4
jneo8 <james851212@gmail.com>
2018-02-25 23:17:36 +0800

    list.go bug fix, prepend pages

--- ptt/list.go
+++ ptt/list.go
@@ -1,8 +1,22 @@
 package ptt
 
 import (
+    // "fmt"
     "github.com/PuerkitoBio/goquery"
+    "github.com/fatih/color"
 )
+
+
+type Page struct {
+    Index int
+    NextUrl  string
+    Url   string
+}
+
+type PageList struct {
+    Pages []*Page
+    Board string
+}
 
 func GetHotBoardList() map[string]string {
     doc := GetDoc(HOT_BOARD_URL)
@@ -17,4 +31,39 @@ func GetHotBoardList() map[string]string {
     })
 
     return hot_board_list
+}
+
+
+func GetAllDocUrl(board string, index int, url string, max int) []*Page{
+    // Gen url.
+    if (index == 0) {
+        url = BASE_URL + "/bbs/" + board + "/index.html"
+    } else {
+        url = BASE_URL + url
+    }
+
+    color.Blue("%s-%d/%d: %s\n", board, index, max, url)
+    pages := make([]*Page, 0)
+
+    if (index < max) {
+        doc := GetDoc(url)
+
+        // Get nextPage
+        nextPage := doc.Find(".action-bar").Find("a:contains('‹ 上頁')")
+
+        if len(nextPage.Nodes) > 0 {
+            href, _ := nextPage.Attr("href")
+            index += 1
+            now_page := &Page{Index: index, Url: url, NextUrl: href}
+            color.Red("%s\n", now_page)
+
+            next_pages := GetAllDocUrl(board, index, href, max)
+            pages = append([]*Page{now_page}, next_pages...)
+        }
+    } else {
+        now_page := &Page{Index: index, Url: url, NextUrl: ""}
+        pages = append(pages, now_page)
+    }
+    return pages
+
 }
