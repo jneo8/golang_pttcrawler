1277250cab1d3e30a76b45d348e2e1181fc5ca7c
jneo8 <james851212@gmail.com>
2018-03-13 05:58:00 +0800

    Finish GetAllDocUrl function.

--- ptt/list.go
+++ ptt/list.go
@@ -11,6 +11,7 @@ type Page struct {
     Index int
     NextUrl  string
     Url   string
+    Doc   *goquery.Document
 }
 
 type PageList struct {
@@ -33,18 +34,30 @@ func GetHotBoardList() map[string]string {
     return hot_board_list
 }
 
-func GetAllDocUrl(board string, index int, url string, max int) {
-    pages := GetAllPageUrl(board, index, url, max)
+func GetAllDocUrl(board string, index int, url string, max int) []string {
+    // Get page's doc.
+    pages := GetPages(board, index, url, max)
 
+    urls := make([]string, 0)
+
+    // Get urls in every page
     for index, page := range pages {
-        color.Red("%d: %s", index, page.Url)
+        color.Red("%d: Get url in %s\n", index, page.Url)
+        if (page.Doc != nil) {
+            page.Doc.Find(".r-ent").Each(func(i int, s *goquery.Selection) {
+                a := s.Find(".title").Find("a")
+                if len(a.Nodes) > 0 {
+                    href, _ := a.Attr("href")
+                    href = ARTICLE_BASE_URL + href
+                    urls = append(urls, href)
+                }
+            })
+        }
     }
-    pagelist := PageList{Board: board, Pages: pages}
-    color.Cyan("%s", pagelist)
-
+    return urls
 }
 
-func GetAllPageUrl(board string, index int, url string, max int) []*Page{
+func GetPages(board string, index int, url string, max int) []*Page{
     // Gen url.
     if (index == 0) {
         url = BASE_URL + "/bbs/" + board + "/index.html"
@@ -53,28 +66,28 @@ func GetAllPageUrl(board string, index int, url string, max int) []*Page{
         url = BASE_URL + url
     }
 
-    color.Blue("%s-%d/%d: %s\n", board, index, max, url)
+    color.Blue(" Start get: %s-%d/%d: %s\n", board, index, max, url)
     pages := make([]*Page, 0)
+    doc := GetDoc(url)
 
     if (index < max) {
-        doc := GetDoc(url)
-
         // Get nextPage
         nextPage := doc.Find(".action-bar").Find("a:contains('‹ 上頁')")
 
         if len(nextPage.Nodes) > 0 {
             href, _ := nextPage.Attr("href")
             index += 1
-            now_page := &Page{Index: index, Url: url, NextUrl: href}
+            now_page := &Page{Index: index, Url: url, NextUrl: href, Doc: doc}
 
-            next_pages := GetAllPageUrl(board, index, href, max)
+            next_pages := GetPages(board, index, href, max)
             pages = append([]*Page{now_page}, next_pages...)
         } else {
-            now_page := &Page{Index: index, Url: url, NextUrl: ""}
+            now_page := &Page{Index: index, Url: url, Doc: doc}
             pages = append(pages, now_page)
         }
     } else {
-        now_page := &Page{Index: index, Url: url, NextUrl: ""}
+
+        now_page := &Page{Index: index, Url: url, Doc: doc}
         pages = append(pages, now_page)
     }
     return pages
