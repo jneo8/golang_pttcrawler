c3d1173e376a9fe4ffc4f6eaf63eaf5628d276df
jneo8 <james851212@gmail.com>
2018-03-14 16:20:32 +0800

    sample.go update

--- sample.go
+++ sample.go
@@ -3,17 +3,19 @@ package main
 import (
     "fmt"
     "./ptt"
+    "github.com/fatih/color"
 )
 
 func main() {
     // ptt.GetTitleList("Gossiping")
     // printhotboard()
-    getalldocurl()
+    // getalldocurl()
+    pool()
 }
 
 
 func getalldocurl() {
-    board := ptt.GetBoard("Gossiping", 0, "", 10)
+    board := ptt.GetBoard("Gossiping", 10)
     for index, url := range board.Urls {
         fmt.Printf("%d: %s\n", index, url)
     }
@@ -27,4 +29,16 @@ func printhotboard() {
         fmt.Printf("%s: %s\n", name, url)
     }
 
+}
+
+func pool() {
+    color.Red("Sample: Pool\n")
+    p := ptt.NewPool()
+    p.AddFish("Gossiping", 3)
+
+    for index, f := range p.Fishes {
+        color.Red("%d: %#v\n", index, f)
+        f.Swim()
+        color.Red("%d: %#v\n", index, f)
+    }
 }
