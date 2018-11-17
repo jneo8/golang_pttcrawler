9c0aea6956e31c1115931ff88c9e1d5313397236
jneo8 <jneo8lin@gmail.com>
2018-11-17 14:02:19 +0800

    init main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/onrik/logrus/filename"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.DebugLevel)
+	log.AddHook(filename.NewHook())
+
+}
+
+func main() {
+	log.Debugf("123")
+}
