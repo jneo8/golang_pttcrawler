29b70c2bd7c31130794ae7ae249658c5a34270c3
jneo8 <jneo8lin@gmail.com>
2018-11-17 15:23:36 +0800

    crawler core

---
+++ crawler/core.go
@@ -0,0 +1,32 @@
+package crawler
+
+import (
+	log "github.com/sirupsen/logrus"
+	"sync"
+)
+
+func Init() {
+	boards := GetHotBoardList()
+
+	boardChan := make(chan string, 10)
+
+	wg := sync.WaitGroup{}
+	for i := 1; i <= 2; i++ {
+		wg.Add(1)
+		go crawlerWorker(&wg, i, boardChan)
+	}
+
+	for _, board := range boards {
+		boardChan <- board
+	}
+	// stop give job.
+	close(boardChan)
+	wg.Wait()
+}
+
+func crawlerWorker(wg *sync.WaitGroup, workerID int, boardChan <-chan string) {
+	defer wg.Done()
+	for board := range boardChan {
+		log.Debugf("worker: %v, %v", workerID, board)
+	}
+}
---
+++ crawler/struct.go
@@ -0,0 +1,5 @@
+package crawler
+
+type BoardPage struct {
+	Urls []string
+}
