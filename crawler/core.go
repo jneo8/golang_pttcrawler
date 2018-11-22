63cab83e9dc3cdfc8ff566d5c8f02eb6b223d7b1
jneo8 <jneo8lin@gmail.com>
2018-11-23 00:08:25 +0800

    use chan to pass url to article process.

--- crawler/core.go
+++ crawler/core.go
@@ -10,7 +10,7 @@ func Init() {
 	boards := GetHotBoardList()
 	boardChan := make(chan Board, 10)
 	wg := sync.WaitGroup{}
-	for i := 1; i <= 2; i++ {
+	for i := 1; i <= 10; i++ {
 		wg.Add(1)
 		go crawlerWorker(&wg, i, boardChan)
 	}
@@ -26,15 +26,24 @@ func Init() {
 func crawlerWorker(wg *sync.WaitGroup, workerID int, boardChan <-chan Board) {
 	defer wg.Done()
 	for board := range boardChan {
+		urlChan := make(chan string)
 		jobWg := sync.WaitGroup{}
-		jobWg.Add(1)
+		jobWg.Add(2)
 		log.Debugf("worker: %v, %v", workerID, board)
-		board.getUrls(&jobWg)
+		go board.getUrls(&jobWg, urlChan)
+		go board.getArticle(&jobWg, urlChan)
 		jobWg.Wait()
 	}
 }
 
-func (board *Board) getUrls(wg *sync.WaitGroup) {
+func (board *Board) getArticle(wg *sync.WaitGroup, urlChan <-chan string) {
+	defer wg.Done()
+	for url := range urlChan {
+		log.Debugf("article: %v", url)
+	}
+}
+
+func (board *Board) getUrls(wg *sync.WaitGroup, urlChan chan<- string) {
 	defer wg.Done()
 	log.Debugf("%v", board)
 
@@ -42,18 +51,18 @@ func (board *Board) getUrls(wg *sync.WaitGroup) {
 
 	// For now it is for test, for production we will check if data in DB.
 	for idx := 1; idx <= 10; idx++ {
+		// Get article url in page.
 		doc.Find(".r-ent").Each(
 			func(i int, s *goquery.Selection) {
 				a := s.Find(".title").Find("a")
 				if len(a.Nodes) > 0 {
 					articleHref, _ := a.Attr("href")
 					articleHref = ARTICLE_BASE_URL + articleHref
-					log.Debugf("Article: %v", articleHref)
-					board.Urls = append(board.Urls, articleHref)
+					urlChan <- articleHref
 				}
 			},
 		)
-		// nextPage
+		// NextPage
 		nextPage := doc.Find(".action-bar").Find("a:contains('‹ 上頁')")
 		if len(nextPage.Nodes) > 0 {
 			nextPageHref, _ := nextPage.Attr("href")
@@ -65,5 +74,5 @@ func (board *Board) getUrls(wg *sync.WaitGroup) {
 			break
 		}
 	}
-	log.Debug(board)
+	close(urlChan)
 }
